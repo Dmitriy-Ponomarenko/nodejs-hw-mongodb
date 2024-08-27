06dc24f96c6e52ea6ed7246df1726a329de2aa2a
Yumfii <144070576+Yumfii@users.noreply.github.com>
2024-08-27 15:49:13 +0200

    bug fix

--- src/server.js
+++ src/server.js
@@ -23,19 +23,6 @@ export const setupServer = () => {
     }),
   );
 
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
-
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
-
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
 
@@ -60,6 +47,19 @@ export const setupServer = () => {
       status: 200,
       message: 'Successfully found contact with id {contactId}!',
       data: contact,
+    });
+  });
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
     });
   });
 
