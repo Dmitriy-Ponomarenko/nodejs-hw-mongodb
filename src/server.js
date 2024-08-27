adf21b61de41e0a6a38e143904643acaebb8abdf
Yumfii <144070576+Yumfii@users.noreply.github.com>
2024-08-27 15:06:35 +0200

    Contact Finding System

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import pino from 'pino-http';
 import cors from 'cors';
 
 import { env } from './utils/env.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -32,6 +33,33 @@ export const setupServer = () => {
     res.status(500).json({
       message: 'Something went wrong',
       error: err.message,
+    });
+  });
+
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    const { contactsId } = req.params;
+    const contact = await getContactById(contactsId);
+
+    if (!contact) {
+      res.status(404).json({
+        message: 'Student not found',
+      });
+      return;
+    }
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contact with id {contactId}!',
+      data: contact,
     });
   });
 
